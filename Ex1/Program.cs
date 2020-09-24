7ae79395b27c37d093df8dbfaec1e15c77ac5395
bruno-f-dias <brunofernandesdias@gmail.com>
2020-09-24 22:10:41 +0100

    Add files via upload

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -6,50 +6,53 @@ namespace Ex1
     {
         static void Main(string[] args)
         {
-
-            string num1, num2;
-            Console.WriteLine("Selecione uma operação:");
-            Console.WriteLine("\n+. Adição;\n-. Subtração\n*. Multiplicação;\n/. Divisão.\n");
-            string op = Console.ReadLine();
-            if (op == "+")
+            string op;
+            do
             {
-                Console.WriteLine("Escreva o primeiro número: ");
-                num1 = Console.ReadLine();
-                Console.WriteLine("Escreva o segundo número: ");
-                num2 = Console.ReadLine();
-                Console.WriteLine("Resultado: " + (Int32.Parse(num1) + Int32.Parse(num2)));
-            }
-            else if (op == "-")
-            {
-                Console.WriteLine("Escreva o primeiro número: ");
-                num1 = Console.ReadLine();
-                Console.WriteLine("Escreva o segundo número: ");
-                num2 = Console.ReadLine();
-                Console.WriteLine("Resultado: " + (Int32.Parse(num1) - Int32.Parse(num2)));
-            }
-            else if (op == "*")
-            {
-                Console.WriteLine("Escreva o primeiro número: ");
-                num1 = Console.ReadLine();
-                Console.WriteLine("Escreva o segundo número: ");
-                num2 = Console.ReadLine();
-                Console.WriteLine("Resultado: " + (Int32.Parse(num1) * Int32.Parse(num2)));
-            }
-            else if (op == "/")
-            {
-                Console.WriteLine("Escreva o primeiro número: ");
-                num1 = Console.ReadLine();
-                Console.WriteLine("Escreva o segundo número: ");
-                num2 = Console.ReadLine();
-                if (num2 != "0")
+                string num1, num2;
+                Console.WriteLine("Selecione uma operação:");
+                Console.WriteLine("\n+. Adição;\n-. Subtração\n*. Multiplicação;\n/. Divisão.\ns. Sair");
+                op = Console.ReadLine();
+                if (op == "+")
                 {
-                    Console.WriteLine("Resultado: " + (Int32.Parse(num1) / Int32.Parse(num2)));
+                    Console.WriteLine("Escreva o primeiro número: ");
+                    num1 = Console.ReadLine();
+                    Console.WriteLine("Escreva o segundo número: ");
+                    num2 = Console.ReadLine();
+                    Console.WriteLine("Resultado: " + (Int32.Parse(num1) + Int32.Parse(num2)));
                 }
-                else
+                else if (op == "-")
                 {
-                    Console.WriteLine("Não é possível dividir por zero");
+                    Console.WriteLine("Escreva o primeiro número: ");
+                    num1 = Console.ReadLine();
+                    Console.WriteLine("Escreva o segundo número: ");
+                    num2 = Console.ReadLine();
+                    Console.WriteLine("Resultado: " + (Int32.Parse(num1) - Int32.Parse(num2)));
                 }
-            }
+                else if (op == "*")
+                {
+                    Console.WriteLine("Escreva o primeiro número: ");
+                    num1 = Console.ReadLine();
+                    Console.WriteLine("Escreva o segundo número: ");
+                    num2 = Console.ReadLine();
+                    Console.WriteLine("Resultado: " + (Int32.Parse(num1) * Int32.Parse(num2)));
+                }
+                else if (op == "/")
+                {
+                    Console.WriteLine("Escreva o primeiro número: ");
+                    num1 = Console.ReadLine();
+                    Console.WriteLine("Escreva o segundo número: ");
+                    num2 = Console.ReadLine();
+                    if (num2 != "0")
+                    {
+                        Console.WriteLine("Resultado: " + (Int32.Parse(num1) / Int32.Parse(num2)));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Não é possível dividir por zero");
+                    }
+                }
+            } while (op=="s");
         }
     }
 }

b1fb3edaad8b3e7ce8704e69cf19a2bc7f8baca9
bruno-f-dias <brunofernandesdias@gmail.com>
2020-09-24 22:36:42 +0100

    Add files via upload

---
+++ Ex4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Jogadores");
+            Console.WriteLine("\n Nome                 Posição     Clube             Golos marcados   Golos sofridos");
+            StreamReader sr = new StreamReader("dados.txt");
+
+            int s = 0;
+            string linha = sr.ReadLine();
+
+            while (linha != "" & linha != null)
+            {
+                string[] aux = linha.Split('|');
+
+                Console.WriteLine("\n" + aux[0] + "   " + aux[1] + "      " + aux[2] + "      " + aux[3] + "      " + aux[4]);
+            }
+        }
+    }
+}
